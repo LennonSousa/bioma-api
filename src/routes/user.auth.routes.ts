a15456acc17028d3feb1f3a7917780d5776102c8
lennonsousa <lennonsousa@outlook.com>
2021-05-08 17:31:03 -0300

    Routes.

--- src/routes/user.auth.routes.ts
+++ src/routes/user.auth.routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 
-import CustomersController from '../controllers/CustomersController'
+import CustomersController from '../controllers/CustomersController';
+import ProjectsController from '../controllers/ProjectsController';
 
 const userAuthRoutes = express.Router();
 
@@ -9,5 +10,11 @@ userAuthRoutes.get('/customers/:id', CustomersController.show);
 userAuthRoutes.post('/customers', CustomersController.create);
 userAuthRoutes.put('/customers/:id', CustomersController.update);
 userAuthRoutes.delete('/customers/:id', CustomersController.delete);
+
+userAuthRoutes.get('/projects', ProjectsController.index);
+userAuthRoutes.get('/projects/:id', ProjectsController.show);
+userAuthRoutes.post('/projects', ProjectsController.create);
+userAuthRoutes.put('/projects/:id', ProjectsController.update);
+userAuthRoutes.delete('/projects/:id', ProjectsController.delete);
 
 export default userAuthRoutes;
