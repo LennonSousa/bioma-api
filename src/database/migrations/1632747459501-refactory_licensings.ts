5851c8a32170d05952b2d0b0d4dc3192c8025bb6
lennonsousa <lennonsousa@outlook.com>
2021-09-27 16:04:05 -0300

    Refixed refactory licensings.

--- src/database/migrations/1632747459501-refactory_licensings.ts
+++ src/database/migrations/1632747459501-refactory_licensings.ts
@@ -4,7 +4,9 @@ export class refactoryLicensings1632747459501 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`licensings\` DROP FOREIGN KEY \`ProjectLineOnLicensing\`,` +
-            ` DROP FOREIGN KEY \`BankOnLicensing\`; ALTER TABLE \`licensings\` DROP COLUMN \`bank_id\`,` +
+            ` DROP FOREIGN KEY \`BankOnLicensing\`;`);
+
+        await queryRunner.query(`ALTER TABLE \`licensings\` DROP COLUMN \`bank_id\`,` +
             ` DROP COLUMN \`project_line_id\`, DROP INDEX \`BankOnLicensing\` , DROP INDEX \`ProjectLineOnLicensing\`;`);
     }
 
